fa149527d324cddc976cf3419313681336aa776b
mthiaener <53690626+mthiaener@users.noreply.github.com>
2025-01-08 17:11:18 +0100

    rebuild from bikeservice

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-from datetime import datetime
+from datetime import datetime, date
 
 from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+from sqlalchemy import create_engine, MetaData, Table, ForeignKey, Column, String, Integer, Date, CHAR, CheckConstraint, join, DATETIME, func, text, select, PrimaryKeyConstraint, Identity, Sequence, case
+from sqlalchemy.orm import sessionmaker, relationship, registry
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
@@ -30,98 +32,124 @@ app.config.update(
 db = SQLAlchemy(app)
 
 # 4) Initialize a Flask App to use for the DATABASE
-db.init_app(app)
+#db.init_app(app)
 
 # Enable Flask-Migrate commands "flask db init/migrate/upgrade" to work
 migrate = Migrate(app, db)
 
 # The import must be done after db initialization due to circular import issue
-from models import Restaurant, Review
+from models import Bikes, Fahrten, Wartung, Wetter 
 
-@app.route('/', methods=['GET'])
-def index():
-    print('Request for index page received')
-    restaurants = Restaurant.query.all()
-    return render_template('index.html', restaurants=restaurants)
 
-@app.route('/<int:id>', methods=['GET'])
-def details(id):
-    restaurant = Restaurant.query.where(Restaurant.id == id).first()
-    reviews = Review.query.where(Review.restaurant == id)
-    return render_template('details.html', restaurant=restaurant, reviews=reviews)
+# Page content / Reports
 
-@app.route('/create', methods=['GET'])
-def create_restaurant():
-    print('Request for add restaurant page received')
-    return render_template('create_restaurant.html')
+# Table joins ( .select_from(<join>))
+j_bf = join (Fahrten, Bikes, Bikes.bike == Fahrten.bike)
+j_bw = join (Bikes, Wartung, Bikes.bike == Wartung.bike)
+j_fw = join (Fahrten, Wetter, Fahrten.wetter == Wetter.bedingung)
+j_wf = join(Fahrten, Wartung, Fahrten.bike == Wartung.bike and Fahrten.datum == Wartung.datum )
 
-@app.route('/add', methods=['POST'])
-@csrf.exempt
-def add_restaurant():
-    try:
-        name = request.values.get('restaurant_name')
-        street_address = request.values.get('street_address')
-        description = request.values.get('description')
-    except (KeyError):
-        # Redisplay the question voting form.
-        return render_template('add_restaurant.html', {
-            'error_message': "You must include a restaurant name, address, and description",
-        })
-    else:
-        restaurant = Restaurant()
-        restaurant.name = name
-        restaurant.street_address = street_address
-        restaurant.description = description
-        db.session.add(restaurant)
+#old_wb=text("""select f.bike, case  when (select count (wartung_id) from wartung w2 where w2.bike = f.bike and w2.datum > (select max (w3.datum) from wartung w3 where w3.bike = f.bike and w3.service ='heiß')) = 0 then 0 else (select sum(f2.strecke) from fahrten f2 where f2.bike = f.bike and f2.datum >(select max(w2.datum) from wartung w2 where w2.bike = f.bike and w2.service ='kalt')) end as "Strecke seit kalt", (select sum(f3.strecke) from fahrten f3 where f3.bike = f.bike and f3.datum >(select max(w2.datum) from wartung w2 where w2.bike = f.bike and w2.service ='heiß')) as "Strecke seit heiß", case when (select count (wartung_id) from wartung w2 where w2.bike = f.bike and w2.datum > (select max (w3.datum) from wartung w3 where w3.bike = f.bike and w3.service ='heiß')) = 0 then 0  else (select sum(f4.strecke * w.faktor) from fahrten f4  join wetter w on f4.wetter = w.bedingung where f4.bike = f.bike and f4.datum >(select max(w2.datum) from wartung w2 where w2.bike = f.bike and w2.service ='kalt')) end as "Last seit kalt", (select sum(f5.strecke * w.faktor) from fahrten f5 join wetter w on f5.wetter = w.bedingung  where f5.bike = f.bike and f5.datum >(select max(w2.datum) from wartung w2 where w2.bike = f.bike and w2.service ='heiß')) as "Last seit heiß", (select count (wartung_id) from wartung w2 where w2.bike = f.bike and w2.service='kalt' and w2.datum > (select max (w3.datum) from wartung w3 where w3.bike = f.bike and w3.service ='heiß')) as "Wartungen kalt seit heiß", (select max(w2.datum) from wartung w2 where w2.bike = f.bike and w2.service ='kalt') as "Letzter Service kalt", (select max(w2.datum) from wartung w2 where w2.bike = f.bike and w2.service ='heiß') as "Letzter Service heiß" from fahrten f group by f.bike ;""")
+#old_q_wb= db.session.execute(wb)
+
+def wartungsbedarf():
+    subquery_max_datum_heiss = select(func.max(Wartung.datum)).filter(Wartung.bike == Fahrten.bike, Wartung.service == 'heiß').scalar_subquery()
+    subquery_max_datum_kalt = select(func.max(Wartung.datum)).filter(Wartung.bike == Fahrten.bike, Wartung.service == 'kalt').scalar_subquery()
+    subquery_count_wartung_id_heiss = select(func.count(Wartung.wartung_id)).filter(Wartung.bike == Fahrten.bike, Wartung.datum > subquery_max_datum_heiss).scalar_subquery()
+    subquery_sum_strecke_kalt = select(func.sum(Fahrten.strecke)).filter(Fahrten.bike == Fahrten.bike, Fahrten.datum > subquery_max_datum_kalt).scalar_subquery()
+    subquery_sum_strecke_heiss = select(func.sum(Fahrten.strecke)).filter(Fahrten.bike == Fahrten.bike, Fahrten.datum > subquery_max_datum_heiss).scalar_subquery()
+    subquery_sum_last_kalt = select(func.sum(Fahrten.strecke * Wetter.faktor)).join(Wetter, Fahrten.wetter == Wetter.bedingung).filter(Fahrten.bike == Fahrten.bike, Fahrten.datum > subquery_max_datum_kalt).scalar_subquery()
+    subquery_sum_last_heiss = select(func.sum(Fahrten.strecke * Wetter.faktor)).join(Wetter, Fahrten.wetter == Wetter.bedingung).filter(Fahrten.bike == Fahrten.bike, Fahrten.datum > subquery_max_datum_heiss).scalar_subquery()
+    subquery_count_wartung_id_kalt = select(func.count(Wartung.wartung_id)).filter(Wartung.bike == Fahrten.bike, Wartung.service == 'kalt', Wartung.datum > subquery_max_datum_heiss).scalar_subquery()
+    subquery_max_datum_kalt_service = select(func.max(Wartung.datum)).filter(Wartung.bike == Fahrten.bike, Wartung.service == 'kalt').scalar_subquery()
+    subquery_max_datum_heiss_service = select(func.max(Wartung.datum)).filter(Wartung.bike == Fahrten.bike, Wartung.service == 'heiß').scalar_subquery()
+    # Define the main query
+    q_wb = db.session.query(
+    Fahrten.bike,
+    case(
+        [
+            (subquery_count_wartung_id_heiss == 0, 0),
+        ],
+        else_=subquery_sum_strecke_kalt
+    ).label("Strecke seit kalt"),
+    subquery_sum_strecke_heiss.label("Strecke seit heiß"),
+    case(
+        [
+            (subquery_count_wartung_id_heiss == 0, 0),
+        ],
+        else_=subquery_sum_last_kalt
+    ).label("Last seit kalt"),
+    subquery_sum_last_heiss.label("Last seit heiß"),
+    subquery_count_wartung_id_kalt.label("Wartungen kalt seit heiß"),
+    subquery_max_datum_kalt_service.label("Letzter Service kalt"),
+    subquery_max_datum_heiss_service.label("Letzter Service heiß")
+    ).group_by(Fahrten.bike)
+    return q_wb
+
+# Routes
+@app.route("/")
+def home():
+    print('Request for home page received')
+    return render_template("home.html")
+# New functions
+@app.route("/fahrten/", methods=['GET', 'POST'])
+def fahrten():
+    #if request.method =='POST':
+     #   print('Request for add fahrt on fahrten received')
+      #  lfndnr = request.form['lfndnr']
+       # fahrt_to_delete = db.session.query(Fahrten).filter(Fahrten.fahrt_id == "lfndnr")
+        #session.delete(fahrt_to_delete)
+        #db.session.commit()
+        #return redirect(f"/fahrten")
+    #else:
+     #   print('Request for fahrten page received')
+    fahrten_headings = ("Laufende Nr.","Fahrrad", "Datum", "Strecke", "Wetter", "Strava ID")
+    q_list_fahrten = db.session.query(Fahrten.fahrt_id, Fahrten.bike, Fahrten.datum, Fahrten.strecke, Fahrten.wetter, Fahrten.strava_id).order_by(Fahrten.datum.desc()).all()
+    return render_template("fahrten.html", headings=fahrten_headings, data = q_list_fahrten)
+@app.route("/neuefahrt/", methods=['GET', 'POST'])
+def neuefahrt():
+    print('Request for neue fahrt page received')
+    if request.method =='POST':
+        bike = request.form['bike']
+        datum = request.form['datum']
+        strecke = request.form['strecke']
+        wetter = request.form['wetter']
+        strava_id=request.form['strava_id']
+        addfahrt = Fahrten(bike=bike, datum=datum, strecke=strecke, wetter=wetter, strava_id=strava_id)
+        db.session.add(addfahrt)
         db.session.commit()
-
-        return redirect(url_for('details', id=restaurant.id))
-
-@app.route('/review/<int:id>', methods=['POST'])
-@csrf.exempt
-def add_review(id):
-    try:
-        user_name = request.values.get('user_name')
-        rating = request.values.get('rating')
-        review_text = request.values.get('review_text')
-    except (KeyError):
-        #Redisplay the question voting form.
-        return render_template('add_review.html', {
-            'error_message': "Error adding review",
-        })
+        flash('Fahrt erfolgreich hinzugefügt') # wird nicht angezeigt?
+        return redirect(f"/fahrten/")
     else:
-        review = Review()
-        review.restaurant = id
-        review.review_date = datetime.now()
-        review.user_name = user_name
-        review.rating = int(rating)
-        review.review_text = review_text
-        db.session.add(review)
+        return render_template("/neuefahrt.html/")
+@app.route("/neuewartung/", methods=['GET', 'POST'])
+def neuewartung(): 
+    print('Request for neue wartung page received')
+    if request.method =='POST':
+        #wartung_id = request.form['wartung_id']
+        bike = request.form['bike']
+        datum = request.form['datum']
+        service = request.form['service']
+        notes = request.form['notes']
+        addwartung = Wartung( bike=bike, datum=datum, service=service, notes=notes)
+        db.session.add(addwartung) # und das hier auch
         db.session.commit()
-
-    return redirect(url_for('details', id=id))
-
-@app.context_processor
-def utility_processor():
-    def star_rating(id):
-        reviews = Review.query.where(Review.restaurant == id)
-
-        ratings = []
-        review_count = 0
-        for review in reviews:
-            ratings += [review.rating]
-            review_count += 1
-
-        avg_rating = sum(ratings) / len(ratings) if ratings else 0
-        stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 0
-        return {'avg_rating': avg_rating, 'review_count': review_count, 'stars_percent': stars_percent}
-
-    return dict(star_rating=star_rating)
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+        return redirect(f"/wartung/")
+    else:
+        return render_template("/neuewartung.html/")
+@app.route("/wartung/")
+def wartung():
+    print('Request for wartung page received')
+    wartung_headings = ("Laufende Nr.","Fahrrad", "Datum", "Service", "Notizen", "KM-Stand")
+    q_wb_headings = ("Fahrrad", "Strecke seit kalt", "Strecke seit heiß", "Last seit kalt", "Last seit heiß","Wartungen kalt (seit heiß)", "Letzter Service kalt", "Letzter Service heiß")
+    q_list_wartung = db.session.query(Wartung.wartung_id, Wartung.bike, Wartung.datum, Wartung.service, Wartung.notes, func.sum(Fahrten.strecke).filter(Fahrten.datum <= Wartung.datum)).select_from(j_wf).group_by(Wartung.wartung_id).order_by(Wartung.datum.desc()).all()
+    return render_template("wartung.html", headings=q_wb_headings, data=wartungsbedarf(), headings_2=wartung_headings, data_2= q_list_wartung)
+@app.route("/bikes/")
+def bikes():
+    print('Request for bikes page received')
+    bike_headings = ("Fahrrad", "KM-Stand", "Letzte Fahrt")
+    q_list_bikes = db.session.query(Bikes.bike, (Bikes.km_stand + func.sum(Fahrten.strecke)),func.max(Fahrten.datum)).select_from(j_bf).group_by(Bikes.bike).all()
+    return render_template("bikes.html", headings=bike_headings, data=q_list_bikes)
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -3,30 +3,64 @@ from sqlalchemy.orm import validates
 
 from app import db
 
+class Bikes(db.Model):
+    __tablename__ = "bikes"
+    bike = Column("bike", String, primary_key=True, autoincrement=True)
+    km_stand = Column("km_stand", Integer)
+    rel_fahrten = relationship('Fahrten', back_populates='rel_bikes')
+    rel_wartung = relationship('Wartung', back_populates='rel_bikes')
+    def __init__(self, bike, km_stand, rel_fahrten, rel_wartung):
+        self.bike = bike
+        self.km_stand = km_stand
+        self.rel_fahrten = rel_fahrten
+        self.rel_wartung = rel_fahrten
+    def __repr__(self):
+        return f"({self.bike} {self.km_stand} {self.rel_fahrten}{self.rel_wartung})"
 
-class Restaurant(db.Model):
-    __tablename__ = 'restaurant'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    street_address = Column(String(50))
-    description = Column(String(250))
+class Fahrten(db.Model):
+    __tablename__ = "fahrten"
+    fahrt_id = Column("fahrt_id", Integer, primary_key=True,  autoincrement=True)
+    bike = Column("bike", String, ForeignKey(Bikes.bike))
+    datum = Column("datum", DATETIME)
+    strecke = Column("strecke", Integer)
+    wetter = Column("wetter", String)
+    strava_id = Column("strava_id", String)
+    rel_bikes = relationship('Bikes', back_populates='rel_fahrten')
+    def __init__(self, bike, datum, strecke, wetter, strava_id, rel_bikes):
+        self.bike = bike
+        self.datum = datum
+        self.strecke = strecke
+        self.wetter = wetter
+        self.strava_id = strava_id
+        self.rel_bikes = rel_bikes 
+    def __repr__(self):
+        return f"( {self.bike} {self.datum} {self.strecke} {self.wetter} {self.strava_id}{self.rel_bikes})" 
 
-    def __str__(self):
-        return self.name
 
-class Review(db.Model):
-    __tablename__ = 'review'
-    id = Column(Integer, primary_key=True)
-    restaurant = Column(Integer, ForeignKey('restaurant.id', ondelete="CASCADE"))
-    user_name = Column(String(30))
-    rating = Column(Integer)
-    review_text = Column(String(500))
-    review_date = Column(DateTime)
+class Wartung(db.Model):
+    __tablename__="wartung"
+    wartung_id = Column("wartung_id", Integer, primary_key=True ,autoincrement=True)
+    datum = Column("datum", DATETIME)
+    bike = Column("bike", String, ForeignKey(Bikes.bike))
+    service = Column("service", String)
+    notes = Column("notes", String)
+    rel_bikes = relationship('Bikes', back_populates='rel_wartung')
+    def __init__(self, datum, bike, service, notes, rel_bikes):
+        self.datum = datum
+        self.bike = bike
+        self.service = service
+        self.notes = notes
+        self.rel_bikes = rel_bikes
+    def __repr__(self):
+        return f"( {self.datum} {self.bike} {self.service} {self.notes}{self.rel_bikes})"
+        
 
-    @validates('rating')
-    def validate_rating(self, key, value):
-        assert value is None or (1 <= value <= 5)
-        return value
-
-    def __str__(self):
-        return f"{self.user_name}: {self.review_date:%x}"
+class Wetter(db.Model):
+    __tablename__="wetter"
+    bedingung = Column("bedingung", String, primary_key=True)
+    faktor = Column("faktor", Integer)
+    def __init__(self, bedingung, faktor):
+        self.bedingung
+        self.faktor
+    def __repr__(self):
+        return f"({self.bedingung} {self.faktor})"
